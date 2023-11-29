d3d1e5f40afd45af1278773f5f5eb36d386d5e07
heiler <threepebbles@naver.com>
2023-11-30 00:53:53 +0900

    Docs: 상품 도메인 기능 추가

---
+++ src/main/java/vendingmachine/domain/Product.java
@@ -0,0 +1,59 @@
+package vendingmachine.domain;
+
+import vendingmachine.ErrorMessage;
+import vendingmachine.util.Validator;
+
+public class Product {
+    private String name;
+    private int price;
+    private int count;
+
+    public Product(String name, int price, int count) {
+        validateName(name);
+        validatePrice(price);
+        validateCount(count);
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+
+    private void validateCount(int count) {
+        if (count <= 0) {
+            throw new IllegalArgumentException(ErrorMessage.getMessage("상품 개수가 올바르지 않습니다."));
+        }
+    }
+
+    private void validateName(String name) {
+        Validator.checkBlank(name, ErrorMessage.getMessage("상품명이 공백입니다."));
+    }
+
+    private void validatePrice(int price) {
+        if (price < 100 || price % 10 != 0) {
+            throw new IllegalArgumentException(ErrorMessage.getMessage("상품가격이 올바르지 않습니다."));
+        }
+    }
+
+    public void addProduct(int count) {
+        this.count += count;
+    }
+
+    public void subtractProduct(int count) {
+        if (this.count < count) {
+            throw new IllegalArgumentException(ErrorMessage.getMessage("수량이 부족합니다."));
+        }
+        this.count -= count;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
